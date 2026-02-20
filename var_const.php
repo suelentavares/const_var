dbec916e61e5c1641e0f0fa641fbc08f9bf14648
suelentavares <94622266+suelentavares@users.noreply.github.com>
2026-02-20 17:08:21 -0300

    Aula var e const

---
+++ var_const.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-vidth, initial-scale=1.0">
+    <title>Const_var</title>
+</head>
+<body>
+    <?php
+    /* int = numero,
+    string = caractere, 
+    boleano = true/ false,
+    float = 0.0
+    
+    */
+    $aluno = "nome";
+    $idade = "130";
+   // const   inicia com a palavra "define!"
+   // entre aspas duplas
+   // 1º contante
+   //2 º valores
+   define("instituicao", "centro de educação tec. do amazonas");
+
+echo "olá aluno, $aluno  $idade bem vindo ao cetam ".instituicao
+
+
+
+
+?>
+
+</body>
+</html>
